8ef24d1d2c1bd1bb3cb5f80dc44840e18227e75a
Fauzan <fauzanffl031202@gmail.com>
2021-12-29 21:41:21 +0700

    update kodingan

--- src/com/project/main/Main.java
+++ src/com/project/main/Main.java
@@ -1,88 +1,65 @@
 package com.project.main;
 
 import com.project.model.Login;
+import com.project.model.Mobil;
 import com.project.model.Penyewa;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    private static List<Mobil> listMobil;
+    private static Login login;
+    private static Penyewa penyewa;
+    private static Login admin = new Login("admin","root");
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Login login = null;
-        Penyewa penyewa;
+        boolean ulangi = true;
 
-        System.out.println("----------------------------------------");
-        System.out.println("||<===== RENTAL MOBIL AMAOZAN =====>||");
-        System.out.println("----------------------------------------");
+        while (ulangi) {
+            menuAwal();
+            System.out.print("\nSilahkan lakukan = ");
+            int milih = input.nextInt();
 
-        System.out.println();
-        System.out.println("||<=====================================");
-        System.out.println("1. Pendaftaran           ||2. Login     ");
-        System.out.println("||<=====================================");
+            switch (milih) {
+                case 1:
+                    pendaftaran();
 
-        System.out.print("\nSilahkan lakukan = ");
-        int milih = input.nextInt();
+                    ulangi = true;
 
-        switch (milih) {
-            case 1:
-                System.out.println("\n||===== Pendaftaran =====||");
-                System.out.print("Nama = ");
-                String nama = input.next();
+                    break;
 
-                System.out.print("Umur = ");
-                int umur = input.nextInt();
+                case 2:
+                    login();
 
-                System.out.println("Alamat = ");
-                String alamat = input.next();
-
-                System.out.println("Username = ");
-                String user = input.next();
-
-                System.out.println("Password = ");
-                String pass = input.next();
-
-                login = new Login(user,pass);
-                penyewa = new Penyewa(nama,umur,alamat);
-
-                break;
-
-            case 2:
-                System.out.println();
-                System.out.println("\n||===== Login =====||");
-                System.out.print("Username = ");
-                String userlog = input.next();
-
-                System.out.print("Password = ");
-                String passlog = input.next();
-                afterLogin(userlog,passlog,login);
-
-
+                    ulangi = false;
+                    break;
+            }
         }
+
 
     }
 
 
-    public static void afterLogin(String userlog, String passlog, Login login){
+    private static void afterLoginPenyewa(){
         Scanner input = new Scanner(System.in);
-        if (login.getUsername().equals(userlog) && login.getPassword().equals(passlog)){
-            System.out.println("||<=====================================");
-            System.out.println("1. Cek Mobil        ||2. Sewa Mobil     ");
-            System.out.println("3. Pembayaran       ||4. Nota Pembayaran     ");
-            System.out.println("||<=====================================");
+
+            System.out.println("||<=====================================||");
+            System.out.println("1. Cek Mobil    ||    2. Sewa Mobil     ");
+            System.out.println("3. Pembayaran   ||    4. Nota Pembayaran");
+            System.out.println("||<=====================================||");
 
             System.out.print("\nSilahkan lakukan = ");
-            int milih2 = input.nextInt();
+            int milih = input.nextInt();
 
-            switch (milih2) {
+            switch (milih) {
 
                 case 1:
                     System.out.println("\n|| Cek Mobil Ready ||");
-                    System.out.println("1. Avanza");
-                    System.out.println("2. Xenia");
-                    System.out.println("3. Apv");
-                    System.out.println("4. Brio");
-                    System.out.println("5. Jazz");
-                    System.out.println("6. Yaris");
+                    for (int i = 0; i < listMobil.size(); i++) {
+                        System.out.println((i+1)+". "+listMobil.get(i).getJenisMobil());
+                    }
 
                     break;
 
@@ -95,8 +72,100 @@ public class Main {
 
             }
 
-        } else {
+    }
 
+    private static void afterLoginAdmin(){
+
+    }
+
+    private static void menuAwal(){
+        System.out.println("----------------------------------------");
+        System.out.println("||<===== RENTAL MOBIL AMAOZAN =====>||");
+        System.out.println("----------------------------------------");
+
+        System.out.println();
+        System.out.println("||=====================================||");
+        System.out.println("1. Pendaftaran\n2. Login     ");
+        System.out.println("||=====================================||");
+    }
+
+    private static void pendaftaran(){
+        Scanner input = new Scanner(System.in);
+        clearScreen();
+
+        System.out.println("\n||===== Pendaftaran =====||");
+        System.out.print("Nama = ");
+        String nama = input.next();
+
+        System.out.print("Umur = ");
+        int umur = input.nextInt();
+
+        System.out.print("Alamat = ");
+        String alamat = input.next();
+
+        System.out.print("Username = ");
+        String user = input.next();
+
+        System.out.print("Password = ");
+        String pass = input.next();
+
+        login = new Login(user, pass);
+        penyewa = new Penyewa(nama, umur, alamat);
+    }
+
+    private static void login(){
+        Scanner input = new Scanner(System.in);
+        clearScreen();
+
+        System.out.println();
+        System.out.println("\n||===== Login =====||");
+        System.out.print("Username = ");
+        String userlog = input.next();
+
+        System.out.print("Password = ");
+        String passlog = input.next();
+        try {
+            if (login.getUsername().equals(userlog) && login.getPassword().equals(passlog)) {
+                afterLoginPenyewa();
+            } else if (admin.getUsername().equals(userlog) && admin.getPassword().equals(passlog)) {
+                afterLoginAdmin();
+            } else {
+                System.out.println("Username dan password salah!");
+
+                System.out.println();
+                System.out.println("\n||===== Login =====||");
+                System.out.print("Username = ");
+                userlog = input.next();
+
+                System.out.print("Password = ");
+                passlog = input.next();
+                try {
+                    if (login.getUsername().equals(userlog) && login.getPassword().equals(passlog)) {
+                        afterLoginPenyewa();
+                    } else if (admin.getUsername().equals(userlog) && admin.getPassword().equals(passlog)) {
+                        afterLoginAdmin();
+                    } else {
+                        System.out.println("Username dan password salah!");
+
+                    }
+                } catch (NullPointerException e) {
+                    System.out.println("Belum ada data login");
+                }
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Belum ada data login");
+        }
+    }
+
+    private static void clearScreen(){
+        try {
+            if (System.getProperty("os.name").contains("Windows")){
+                new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033\143");
+            }
+        } catch (Exception ex){
+            System.err.println("\nTidak bisa clear scree");
         }
     }
 }
